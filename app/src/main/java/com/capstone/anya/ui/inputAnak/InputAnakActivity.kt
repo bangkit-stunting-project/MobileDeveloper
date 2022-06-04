8e0db1dff185eb1ca72541752610ae2a2add00e0
Hary <noval.ramadan600@gmail.com>
2022-06-04 22:23:53 +0700

    page input anak

---
+++ app/src/main/java/com/capstone/anya/ui/inputAnak/InputAnakActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.anya.ui.inputAnak
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.anya.R
+
+class InputAnakActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input_anak)
+    }
+}
--- app/src/main/java/com/capstone/anya/ui/listAnak/ListAnakActivity.kt
+++ app/src/main/java/com/capstone/anya/ui/listAnak/ListAnakActivity.kt
@@ -1,9 +1,12 @@
 package com.capstone.anya.ui.listAnak
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import com.capstone.anya.R
+import com.capstone.anya.ui.inputAnak.InputAnakActivity
 
 class ListAnakActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,5 +21,16 @@ class ListAnakActivity : AppCompatActivity() {
         val inflater = menuInflater
         inflater.inflate(R.menu.list_anak, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean{
+        when(item.itemId){
+            R.id.navInputAnak -> {
+                val menuInputAnak = Intent(this, InputAnakActivity::class.java)
+                startActivity(menuInputAnak)
+                return true
+            }
+            else -> return true
+        }
     }
 }
